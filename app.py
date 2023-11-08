e7b955b6076e0e0ce4088f87170bca38f63c1b8c
Jessie Leal <jessie.leal01@utrgv.edu>
2023-11-07 21:37:27 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+filepath = "/Users/jessie/PycharmProjects/Flask/static/aboutMe.txt"
+
+# Function to read in details for page
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+# Make a homepage
+@app.route('/')
+def homepage():
+    aboutme = readDetails(filepath)
+    return render_template('base.html', name="Jessie", aboutMe = aboutme)
+
+@app.route('/hello/<name>')
+def hello(name):
+    listOfNames = [name, "Yoyo", "Yennifer"]
+    return render_template('name.html', Sname=name, nameList=listOfNames)
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo(name = None):
+    if request.method == 'POST':
+        name=request.form['name']
+    return render_template('form.html', name=name)
+
+# Function to read in details for page
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+# Add the option to run this file directly
+if __name__== "__main__":
+    app.run(debug=True)
