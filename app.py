57bc6b6e921c4c369cdd94a00dbdaf3b1631fba8
Jessie Leal <jdleal86@gmail.com>
2023-11-14 13:45:07 -0600

    updated requirements

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#setup app to use an SQLalchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sampledb.db'
+db = SQLAlchemy(app) #database attached to app
+
+#setup a simple table for database
+class Visitor(db.Model):
+    # need to put what type of data is in each column, and set unique value as primary key
+    username = db.Column(db.String(100), primary_key = True)
+    numVisits = db.Column(db.Integer, default = 1)
+
+    def __repr__(self):
+        return f"{self.username} - {self.numVisits}"
+
+#Create tables in Database
+with app.app_context():
+    db.create_all()
 
 
 filepath = "static/aboutMe.txt"
@@ -25,7 +43,27 @@ def hello(name):
 def formDemo(name = None):
     if request.method == 'POST':
         name=request.form['name']
+        #Check if user is in the database
+        visitor = Visitor.query.get(name)
+        #if it doesn't find the name, visitor will be "none"
+        if visitor == None:
+            #Add the Visitor to the database
+            visitor = Visitor(username = name) #create the visitor object
+            #add visitor to the database
+            db.session.add(visitor)
+        else:
+            visitor.numVisits += 1
+
+        db.session.commit()
+
     return render_template('form.html', name=name)
+
+@app.route('/visitors')
+def visitors():
+    #query the database to find all visitors
+    people = Visitor.query.all()
+    return render_template('visitors.html', people = people)
+
 
 # Function to read in details for page
 def readDetails(filepath):
